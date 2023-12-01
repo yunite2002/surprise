14cf32dabfc3fd0a9bd6f6b4f978c6e3101d0397
yunite2002 <151495337+yunite2002@users.noreply.github.com>
2023-12-01 13:48:19 +1300

    Update countdown.js

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "02/06/",
+        dayMonth = "02/07/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
